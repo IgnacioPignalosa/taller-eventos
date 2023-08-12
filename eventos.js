b2a2cde66cfa1240dcd89d9bb3550b0821688736
IgnacioPignalosa <ipj1994@gmail.com>
2023-08-12 17:59:20 -0300

    hola div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const divbutt = document.getElementById("divbutt");
+
+    divbutt.addEventListener("click", function () {
+        alert('Hola! Soy el div');
+    });
+});
