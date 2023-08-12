d4debefe45070d452b39d7ae517513bf40197af3
IgnacioPignalosa <ipj1994@gmail.com>
2023-08-12 18:03:08 -0300

    quinto

--- eventos.js
+++ eventos.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const holadiv = document.getElementsByClassName("divbutt");
+    const holadiv = document.getElementById("divbutt");
 
     holadiv.addEventListener("click", function () {
         alert('Hola! Soy el div');
